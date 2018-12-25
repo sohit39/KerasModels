da455942925a4db6c2ef040506624bd83a46ab04
sohit39 <gatiganti44914@sas.edu.sg>
2018-12-25 12:23:06 +0800

    added GUI

---
+++ gui.py
@@ -0,0 +1,44 @@
+from tkinter import filedialog
+from tkinter import *
+from tkinter import messagebox
+from PIL import Image, ImageTk
+import numpy as np
+from keras.preprocessing import image
+from keras.models import Sequential
+from keras.models import load_model
+
+classifier = Sequential()
+model = load_model(
+    "catdogmodelNew.h5",
+    custom_objects=None,
+    compile=True
+)
+while(True) :
+    root = Tk()
+    root.filename =  filedialog.askopenfilename(initialdir = "/Users/sohitgatiganti/",title = "Select file",filetypes = (("jpeg files","*.jpg"),("all files","*.*")))
+    print (root.filename)
+    test_image = image.load_img(root.filename, target_size = (64, 64))
+    test_image = image.img_to_array(test_image)
+    test_image = np.expand_dims(test_image, axis = 0)
+    result = model.predict(test_image)
+    print(result)
+    #training_set.class_indices
+    if result[0][0] == 1:
+        prediction = 'dog'
+    else:
+        prediction = 'cat'
+
+
+    print(prediction)
+    original = Image.open(root.filename)
+    render = ImageTk.PhotoImage(original)
+    img = Label(image = render)
+    img.image = render
+    img.place(x=0,y=0)
+    text = Label(text=prediction, anchor=S)
+    text.place(x=50,y=50)
+    text.config(font=("Courier", 44))
+    text.pack()
+    root.title("Image Classifier")
+    root.geometry(str((render.width()+10)) + "x" + str((render.height()+10)))
+    root.mainloop()
